b0d0fad1d9d195a23e09b78ef6366d436eb21b1a
gitchat371 <gptc55395@gmail.com>
2026-02-12 13:26:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+// Form submit message
 document.getElementById("admissionForm").addEventListener("submit", function(event){
     event.preventDefault();
     document.getElementById("message").innerText = "Thanks for submitting record";
     document.getElementById("admissionForm").reset();
+});
+
+// Dropdown toggle
+const dropdowns = document.querySelectorAll(".dropdown-btn");
+
+dropdowns.forEach(btn => {
+    btn.addEventListener("click", function() {
+        const content = this.nextElementSibling;
+        content.style.display = content.style.display === "block" ? "none" : "block";
+    });
 });
