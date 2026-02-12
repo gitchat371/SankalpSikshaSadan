a09998d330d43347e3925c762e5402b114050967
gitchat371 <gptc55395@gmail.com>
2026-02-12 13:28:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.getElementById("admissionForm").addEventListener("submit", function(event){
     event.preventDefault();
     document.getElementById("message").innerText = "Thanks for submitting record";
-    document.getElementById("admissionForm").reset();
+    this.reset();
 });
 
 // Dropdown toggle
