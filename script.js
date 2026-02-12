b6d2397c72dd15a584cd70a8e81f1a460d63483c
gitchat371 <gptc55395@gmail.com>
2026-02-12 13:13:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById("admissionForm").addEventListener("submit", function(event){
     event.preventDefault();
-    document.getElementById("message").innerText = "Form Submitted Successfully!";
+    document.getElementById("message").innerText = "Thanks for submitting record";
+    document.getElementById("admissionForm").reset();
 });
